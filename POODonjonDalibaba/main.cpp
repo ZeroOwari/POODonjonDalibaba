2db5c208c00aeb1451df10887db6325fdceeb4f2
Lukas Prêtre <lukas.pretre@viacesi.fr>
2024-11-28 09:54:56 +0100

    Main

--- POODonjonDalibaba/main.cpp
+++ POODonjonDalibaba/main.cpp
@@ -1,20 +1,35 @@
-// POODonjonDalibaba.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
+#include "Barbare.hpp"
+#include "Elfe.hpp"
+#include "Humain.hpp"
+#include "Nain.hpp"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Barbare b(110, 50);
+    Elfe e(150, 40);
+    Humain h(100, 60);
+    Nain n(120, 70);
+
+    b.afficher();
+    //e.afficher();
+    //h.afficher();
+    //n.afficher();
+
+    b.set_pv(120);
+    b.set_force(6);
+    b.set_intelligence(3);
+    b.set_adresse(3);
+    b.set_charisme(4);
+    b.set_courage(4);
+    b.set_poidsmax(60);
+    b.set_poidsactuel(30);
+
+    b.afficher();
+
+    b.ajoutPoint("Force", 5);
+
+    b.afficher();
+
+    return 0;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
