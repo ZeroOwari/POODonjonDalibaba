c320c1560bbad3acb1f42c16c2c15d2afc3beef6
Rémi VASSEUR <remi.vasseur1@viacesi.fr>
2024-12-02 15:24:15 +0100

    fix de magicien

--- POODonjonDalibaba/Magicien.hpp
+++ POODonjonDalibaba/Magicien.hpp
@@ -21,12 +21,12 @@ public:
 
     int attaque_simple(int resistance_cible) {
         int puissance = 30;
-        int force = this->competences["Force"];
+        int adresse = this->competences["Adresse"];
         int facteur_aleatoire = std::rand() % 5;
 
-        int degats = std::max(0, (puissance + force) - resistance_cible + facteur_aleatoire);
+        int degats = std::max(0, (puissance + adresse) - resistance_cible + facteur_aleatoire);
 
-        std::cout << "Le guerrier assène un Coup puissant avec son épée !" << std::endl;
+        std::cout << "Le Magicien Lance une boule de feu!" << std::endl;
         std::cout << "Il inflige " << degats << " points de dégâts." << std::endl;
 
         return degats;
@@ -34,12 +34,12 @@ public:
 
     int attaque_speciale(int resistance_cible) {
         int puissance = 60;
-        int force = this->competences["Force"];
+        int adresse = this->competences["Adresse"];
         int facteur_aleatoire = std::rand() % 10;
 
-        int degats = std::max(0, (puissance + force * 2) - resistance_cible + facteur_aleatoire);
+        int degats = std::max(0, (puissance + adresse * 2) - resistance_cible + facteur_aleatoire);
 
-        std::cout << "Le guerrier libère une Frappe colossale qui écrase l'ennemi !" << std::endl;
+        std::cout << "Le Magicien libère une Puissance incroyable !" << std::endl;
         std::cout << "Il inflige " << degats << " points de dégâts." << std::endl;
 
         return degats;
@@ -47,12 +47,12 @@ public:
 
     int utiliser_competence_speciale(int resistance_cible) {
         int puissance = 100;
-        int force = this->competences["Force"];
+        int adresse = this->competences["Adresse"];
         int facteur_aleatoire = std::rand() % 15;
 
-        int degats = std::max(0, (puissance + force * 3) - resistance_cible + facteur_aleatoire);
+        int degats = std::max(0, (puissance + adresse * 3) - resistance_cible + facteur_aleatoire);
 
-        std::cout << "Le guerrier entre en Furie, frappant avec une force surhumaine !" << std::endl;
+        std::cout << "Le Magicien entre en état de tanse,il frappe son ennemi avec une puissance folle !" << std::endl;
         std::cout << "Il inflige " << degats << " points de dégâts destructeurs." << std::endl;
 
         return degats;
