c3c137760e12e7896d1a8552431f3f47b34480f3
Lukas Prêtre <lukas.pretre@viacesi.fr>
2024-12-02 16:01:03 +0100

    inventaire centré sur le player

--- POODonjonDalibaba/Asset.hpp
+++ POODonjonDalibaba/Asset.hpp
@@ -23,12 +23,13 @@
 
 
 
-void Inventaire(sf::RenderWindow& window) {
+void Inventaire(sf::RenderWindow& window, const Player& player) {
+	sf::Vector2f playerPosition = player.getPosition();
 
+	const float inventoryWidth = 1000;
+	const float inventoryHeight = 300;
 
-
-
-
+	sf::Vector2f inventoryPosition(playerPosition.x - inventoryWidth / 2, playerPosition.y - inventoryHeight / 2);
 	// police d'écriture : 
 	sf::Font Police;
 	if (!Police.loadFromFile("fonts/Police.ttf")) {
@@ -92,38 +93,26 @@ void Inventaire(sf::RenderWindow& window) {
 	}
 	sf::Sprite sprite_Boisson;
 	sprite_Boisson.setTexture(Boisson);
-	//}
-
-
-
-
-
-
-
-
 
 	sf::Text Inventaire_text;
 	Inventaire_text.setFont(Police);
 	Inventaire_text.setString("INVENTAIRE");
 	Inventaire_text.setCharacterSize(30);
 	Inventaire_text.setFillColor(sf::Color::White);
-	Inventaire_text.setPosition(460, 50);
+	Inventaire_text.setPosition(inventoryPosition.x + 10, inventoryPosition.y + 10);
 	window.draw(Inventaire_text);
 
 
-
-	//et la ça marche jsp prq
-
 	sf::RectangleShape Case_Personnage(sf::Vector2f(100, 250));
 	Case_Personnage.setFillColor(sf::Color(255, 255, 255, 0));
-	Case_Personnage.setPosition(1100, 80);
+	Case_Personnage.setPosition(inventoryPosition.x + 650, inventoryPosition.y + 30);
 	Case_Personnage.setOutlineThickness(7);
 	Case_Personnage.setOutlineColor(sf::Color(255, 255, 255));
 	window.draw(Case_Personnage);
 
-	sf::RectangleShape Inventaire(sf::Vector2f(1000, 300));
+	sf::RectangleShape Inventaire(sf::Vector2f(inventoryWidth, inventoryHeight));
 	Inventaire.setFillColor(sf::Color(0, 0, 0, 50));
-	Inventaire.setPosition(450, 50);
+	Inventaire.setPosition(inventoryPosition);
 	Inventaire.setOutlineThickness(7);
 	Inventaire.setOutlineColor(sf::Color(255, 255, 255));
 	window.draw(Inventaire);
@@ -135,16 +124,12 @@ void Inventaire(sf::RenderWindow& window) {
 	PRESS_X.setString("PRESS X TO LEAVE");
 	PRESS_X.setCharacterSize(30);
 	PRESS_X.setFillColor(sf::Color::White);
-	PRESS_X.setPosition(460, 310);
+	PRESS_X.setPosition(inventoryPosition.x + 10, inventoryPosition.y + 210);
 	window.draw(PRESS_X);
 
-
-
-
-
-	int Position_en_y = 100;
+	int Position_en_y = inventoryPosition.y + 50;
 	int MOOVE_DE_Y = 0;
-	int Position_en_x = 450;
+	int Position_en_x = inventoryPosition.x;
 
 	for (int i = 0; i < 2; i++) {
 		int MOOVE_DE_X = 0;
@@ -157,33 +142,30 @@ void Inventaire(sf::RenderWindow& window) {
 			Case_inventaire_Item.setOutlineThickness(7);
 			Case_inventaire_Item.setOutlineColor(sf::Color(255, 255, 255));
 			window.draw(Case_inventaire_Item);
-
-
-
 		}
 		MOOVE_DE_Y = MOOVE_DE_Y + 150;
 	}
-	int Position_y = 70;
+	int Position_y = playerPosition.y + 20;
 	int MOOVE_Y = 0;
 	for (int i = 0; i < 4; i++) {
 		sf::RectangleShape Case_Inventaire_d_equipement(sf::Vector2f(50, 50));
 		Case_Inventaire_d_equipement.setFillColor(sf::Color(255, 255, 255, 0));
-		Case_Inventaire_d_equipement.setPosition(1300, Position_y + MOOVE_Y);
+		Case_Inventaire_d_equipement.setPosition(inventoryPosition.x + 850, Position_y + MOOVE_Y);
 		Case_Inventaire_d_equipement.setOutlineThickness(7);
 		Case_Inventaire_d_equipement.setOutlineColor(sf::Color(255, 255, 255));
 		window.draw(Case_Inventaire_d_equipement);
 		MOOVE_Y = MOOVE_Y + 70;
 	}
-	sprite_casque.setPosition(1300, 70);
+	sprite_casque.setPosition(inventoryPosition.x + 850, inventoryPosition.y + 20);
 	window.draw(sprite_casque);
 
-	sprite_Plastron.setPosition(1300, 140);
+	sprite_Plastron.setPosition(inventoryPosition.x + 850, inventoryPosition.y + 90);
 	window.draw(sprite_Plastron);
 
-	sprite_Pantalon.setPosition(1300, 210);
+	sprite_Pantalon.setPosition(inventoryPosition.x + 850, inventoryPosition.y + 160);
 	window.draw(sprite_Pantalon);
 
-	sprite_Chaussure.setPosition(1300, 280);
+	sprite_Chaussure.setPosition(inventoryPosition.x + 850, inventoryPosition.y + 230);
 	window.draw(sprite_Chaussure);
 	/*
 	if (Objet_de_l_inventaire == true) {
@@ -197,12 +179,12 @@ void Inventaire(sf::RenderWindow& window) {
 	// poids de l'inventaire : +
 
 	sf::Text POIDS_INVENTAIRE;
-	string Poids_de_l_inventaire = getPOIDS();
+	std::string Poids_de_l_inventaire = getPOIDS();
 	POIDS_INVENTAIRE.setFont(Police);
 	POIDS_INVENTAIRE.setString(Poids_de_l_inventaire);
 	POIDS_INVENTAIRE.setCharacterSize(30);
 	POIDS_INVENTAIRE.setFillColor(sf::Color::White);
-	POIDS_INVENTAIRE.setPosition(1400, 310);
+	POIDS_INVENTAIRE.setPosition(inventoryPosition.x + 1000, inventoryPosition.y + 210);
 	window.draw(POIDS_INVENTAIRE);
 
 
